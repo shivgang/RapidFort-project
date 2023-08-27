24e1110dda15d2b2e26b01d522c073be9f58386d
shivgang <gangwarshivang15@gmail.com>
2023-08-27 16:06:41 +0530

    New Commit

--- index.js
+++ index.js
@@ -5,39 +5,47 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 app.use(express.json());
 
-const storage = multer.memoryStorage(); // Store file in memory for simplicity
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Files will be saved in the "uploads" directory
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 const upload = multer({ storage: storage });
+
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 })
+
 app.post('/upload',upload.single('file'),(req,res)=>{
     if (!req.file) {
         console.log(0);
         return res.status(400).send('No file uploaded.');
     }
-    if (req.file) {
-        console.log(1);
-    }
-
-    const fileBuffer = req.file.buffer;
-    const fileName = req.file.originalname;
-    const fileExtension = path.extname(fileName);
-
-    // Assuming the file contains text, you can convert the buffer to string
-    const fileContent = fileBuffer.toString();
 
     const fileInfo = {
-        name: fileName,
-        extension: fileExtension,
+        name: req.file.originalname,
+        extension: path.extname(req.file.originalname),
+        size: Math.round(req.file.size / 1024) + ' KB'
     };
-    console.log(req.file);
 
-    res.json(fileInfo);
+    const fileLink = `<a href="/uploads/${req.file.originalname}" target="_blank">View File</a>`;
+
+    res.send(`
+        <h1>File Details</h1>
+        <p>Name: ${fileInfo.name}</p>
+        <p>Extension: ${fileInfo.extension}</p> 
+        <p>Size: ${fileInfo.size}</p>
+        <p>URL: ${fileLink}</p>
+    `);
 })
 
 
